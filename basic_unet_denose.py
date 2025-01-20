92ade61a9e0f97dfff9cdf03468b8ba6fd5a954a
cn-xvkong <62235642+cn-xvkong@users.noreply.github.com>
2025-01-20 12:56:54 +0800

    Update basic_unet_denose.py

--- basic_unet_denose.py
+++ basic_unet_denose.py
@@ -1,14 +1,3 @@
-# Copyright (c) MONAI Consortium
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#     http://www.apache.org/licenses/LICENSE-2.0
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 from typing import Optional, Sequence, Union
 import math 
 import torch
@@ -243,27 +232,23 @@ class text_classifier(nn.Module):
         # 自适应平均池化，将空间维度（高，宽）池化成1x1
         self.avg_pool = nn.AdaptiveAvgPool2d(1)
 
-        # 处理 num_polyps
         self.fc1 = nn.Sequential(
-            nn.Linear(in_c, in_c // 8, bias=False),  # 将输入通道数减少到 in_c//8
+            nn.Linear(in_c, in_c // 8, bias=False),  
             nn.ReLU(),
-            nn.Linear(in_c // 8, out_c[0], bias=False)  # 输出 num_polyps
+            nn.Linear(in_c // 8, out_c[0], bias=False)  
         )
 
-        # 处理 polyp_sizes
         self.fc2 = nn.Sequential(
-            nn.Linear(in_c, in_c // 8, bias=False),  # 将输入通道数减少到 in_c//8
+            nn.Linear(in_c, in_c // 8, bias=False), 
             nn.ReLU(),
-            nn.Linear(in_c // 8, out_c[1], bias=False)  # 输出 polyp_sizes
+            nn.Linear(in_c // 8, out_c[1], bias=False)  
         )
 
     def forward(self, feats):
-        # 对输入进行自适应平均池化，结果是 (batch_size, channels, 1, 1)
         pool = self.avg_pool(feats).view(feats.shape[0], feats.shape[1])
 
-        # 分别传入两个全连接层
-        num_polyps = self.fc1(pool)  # 预测 polyps 数量
-        polyp_sizes = self.fc2(pool)  # 预测 polyp 的尺寸
+        num_polyps = self.fc1(pool)  
+        polyp_sizes = self.fc2(pool)  
 
         return num_polyps, polyp_sizes
 
@@ -427,54 +412,6 @@ class BasicUNetDe(nn.Module):
         upsample: str = "deconv",
         dimensions: Optional[int] = None,
     ):
-        """
-        A UNet implementation with 1D/2D/3D supports.
-
-        Based on:
-
-            Falk et al. "U-Net – Deep Learning for Cell Counting, Detection, and
-            Morphometry". Nature Methods 16, 67–70 (2019), DOI:
-            http://dx.doi.org/10.1038/s41592-018-0261-2
-
-        Args:
-            spatial_dims: number of spatial dimensions. Defaults to 3 for spatial 3D inputs.
-            in_channels: number of input channels. Defaults to 1.
-            out_channels: number of output channels. Defaults to 2.
-            features: six integers as numbers of features.
-                Defaults to ``(32, 32, 64, 128, 256, 32)``,
-
-                - the first five values correspond to the five-level encoder feature sizes.
-                - the last value corresponds to the feature size after the last upsampling.
-
-            act: activation type and arguments. Defaults to LeakyReLU.
-            norm: feature normalization type and arguments. Defaults to instance norm.
-            bias: whether to have a bias term in convolution blocks. Defaults to True.
-                According to `Performance Tuning Guide <https://pytorch.org/tutorials/recipes/recipes/tuning_guide.html>`_,
-                if a conv layer is directly followed by a batch norm layer, bias should be False.
-            dropout: dropout ratio. Defaults to no dropout.
-            upsample: upsampling mode, available options are
-                ``"deconv"``, ``"pixelshuffle"``, ``"nontrainable"``.
-
-        .. deprecated:: 0.6.0
-            ``dimensions`` is deprecated, use ``spatial_dims`` instead.
-
-        Examples::
-
-            # for spatial 2D
-            >>> net = BasicUNet(spatial_dims=2, features=(64, 128, 256, 512, 1024, 128))
-
-            # for spatial 2D, with group norm
-            >>> net = BasicUNet(spatial_dims=2, features=(64, 128, 256, 512, 1024, 128), norm=("group", {"num_groups": 4}))
-
-            # for spatial 3D
-            >>> net = BasicUNet(spatial_dims=3, features=(32, 32, 64, 128, 256, 32))
-
-        See Also
-
-            - :py:class:`monai.networks.nets.DynUNet`
-            - :py:class:`monai.networks.nets.UNet`
-
-        """
         super().__init__()
         if dimensions is not None:
             spatial_dims = dimensions
@@ -520,17 +457,6 @@ class BasicUNetDe(nn.Module):
         self.final_conv = Conv["conv", spatial_dims](128, out_channels, kernel_size=1)
 
     def forward(self, x: torch.Tensor, t, embeddings=None, image=None, text=None):
-        """
-        Args:
-            x: input should have spatially N dimensions
-                ``(Batch, in_channels, dim_0[, dim_1, ..., dim_N])``, N is defined by `dimensions`.
-                It is recommended to have ``dim_n % 16 == 0`` to ensure all maxpooling inputs have
-                even edge lengths.
-
-        Returns:
-            A torch Tensor of "raw" predictions in shape
-            ``(Batch, out_channels, dim_0[, dim_1, ..., dim_N])``.
-        """
         temb = get_timestep_embedding(t, 128)
         temb = self.temb.dense[0](temb)
         temb = nonlinearity(temb)
